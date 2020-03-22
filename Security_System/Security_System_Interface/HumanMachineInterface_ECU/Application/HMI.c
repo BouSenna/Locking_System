1cc192bc8363c46c5eef8439eb15d57f62fee4ce
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-03-22 07:57:38 +0200

    added a function that displays message to guide the user.
    This function is used to display a message as circularly scrolling text on the first row to guide the user to begin using the system.

--- Security_System/Security_System_Interface/HumanMachineInterface_ECU/Application/HMI.c
+++ Security_System/Security_System_Interface/HumanMachineInterface_ECU/Application/HMI.c
@@ -13,3 +13,25 @@ void initialize_Devices(void){
 	/// Setting the configurations and initializing USART.
 	USART_Init(BaudRate, NO_PARITY, ONE_STOP_BIT, EIGHT_BITS, ASYNCHRONOUS);
 }
+
+void initial_LCDScrollText(void){
+	int i, j, str_size = 30;
+	char temp;
+	char str[30] = "Please press 0 to continue.   ";
+	_delay_ms(1000);
+	for (i = 0; i < str_size - 1; i++) {
+		LCD_SetCursor(0, 0);
+		LCD_WriteString(str);
+
+		/// Storing the value of the first index of the array in temp variable.
+		temp = str[0]; 
+		/// Shift all the characters one place to the left.
+		for (j = 0; j < str_size - 1; j++) {
+			str[j] = str[j + 1];
+		}
+		/// Add the value of the temp variable at the end of the array.
+		str[str_size - 2] = temp;
+		str[str_size - 1] = 0;   // Terminator.
+		_delay_ms(1000);
+	}
+} 
--- Security_System/Security_System_Interface/HumanMachineInterface_ECU/Application/HMI.h
+++ Security_System/Security_System_Interface/HumanMachineInterface_ECU/Application/HMI.h
@@ -22,3 +22,23 @@
  *
  ***/
 void initialize_Devices(void);
+
+/***
+ * [Purpose] Display message to the user to inform him that he should press 0 key to begin using the system. 
+ * [NOTE] The message is displayed as circularly scrolling text on the first row since I'm using 16x2 alphanumeric
+ *        LCD and the message length is 30 character. 
+ * 
+ * Store the message in character array.
+ * Loop with the array size and do the following instructions each iteration:
+ *       [1] Display the message stored in the array on the LCD. 
+ *           (Note that only the first 16 characters will be displayed to the user)
+ *       [2] Store the first character in a temporary variable.
+ *       [3] Shift all the other characters one place to the left.
+ *       [4] Add the value stored in the temporary variable at the end of the array 
+ *           then add the string terminator at the last index of the array.
+ *
+ * [Arguments] None.
+ * [Return Type] Void.
+ *
+ ***/
+void initial_LCDScrollText(void);
